b9cfd466eff6c58ddd875137001683d1a25b92a6
Cleiton Lahm <cleijllahm@gmail.com>
2023-05-02 08:27:38 -0300

    add class calcController

---
+++ scripts/controller/calcController.js
@@ -0,0 +1,24 @@
+class CalcController{
+
+  constructor() {
+    this._displayCalc = "0";
+    this.currentDate;
+  }
+
+  get displayCalc() {
+    return this._displayCalc;
+  }
+
+  set displayCalc(valor) {
+    this._displayCalc = valor;
+  }
+  
+  get dataAtual() {
+    return this.currentDate;
+  }
+
+  set currentDate(valor) {
+    this.currentDate = valor;
+  }
+
+}
